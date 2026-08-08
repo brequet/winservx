e7c36e52a9b54bac58d91c772c97c82555b03f12
brequet <requet.baptiste@gmail.com>
2026-08-08 13:25:56 +0200

    add svelte

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,7 @@
-import { invoke } from "@tauri-apps/api/core";
+import { mount } from "svelte";
+import "./styles.css";
+import App from "./App.svelte";
 
-let greetInputEl: HTMLInputElement | null;
-let greetMsgEl: HTMLElement | null;
+const app = mount(App, { target: document.getElementById("app")! });
 
-async function greet() {
-  if (greetMsgEl && greetInputEl) {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    greetMsgEl.textContent = await invoke("greet", {
-      name: greetInputEl.value,
-    });
-  }
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form")?.addEventListener("submit", (e) => {
-    e.preventDefault();
-    greet();
-  });
-});
+export default app;
